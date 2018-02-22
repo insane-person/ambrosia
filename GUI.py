bf7ca58b950d43e7ea6de2442457e2da6c831096
Царевский Василий Александрович <tsarevskiy@bolid.ru>
2018-02-22 16:34:18 +0300

    add some ui

--- GUI.py
+++ GUI.py
@@ -1,67 +1,11 @@
-from PyQt5 import QtWidgets
-from PyQt5.Qt import *
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog
+from mainWindow import Ui_MainWindow
 
+app = QApplication(sys.argv)
+window = QDialog()
+ui = Ui_MainWindow()
+ui.setupUi(window)
 
-# Наследуемся от QMainWindow
-class MainWindow(QMainWindow):
-    # Переопределяем конструктор класса
-    def __init__(self):
-        # Обязательно нужно вызвать метод супер класса
-        QMainWindow.__init__(self)
-
-        self.setMinimumSize(QSize(640, 480))  # Устанавливаем размеры
-        self.setWindowTitle("Ambrosia")  # Устанавливаем заголовок окна
-
-        tabs = QTabWidget(self)
-
-        tabs.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-
-        product_table = QTableWidget(self)
-        product_table.setRowCount(10)
-        product_table.setColumnCount(5)
-        product_table.setHorizontalHeaderLabels(['Наименование', 'Калорийность', 'Белки', 'Жиры', 'Углеводы'])
-
-        tab2 = QTextEdit(self)
-        tab3 = QTextEdit(self)
-        tab4 = QTextEdit(self)
-
-        tabs.addTab(product_table, "Продукты")
-        tabs.addTab(tab2, "Блюда")
-        tabs.addTab(tab3, "Раскладка")
-        tabs.addTab(tab4, "Закупка")
-
-        hbox = QHBoxLayout()
-        hbox.addWidget(tabs)
-
-        vbox = QVBoxLayout()
-        vbox.addLayout(hbox)
-
-        wid = QWidget(self)
-        self.setCentralWidget(wid)
-
-        wid.setLayout(vbox)
-
-        self.show()
-
-class productTable(QTableWidget):
-    def __init__(self, args):
-        """
-            В конструкторе мы делаем всё, что необходимо для запуска нашего приложения, которое
-            создаёт QApplication в __init__ методе, затем добваляет наши виджеты и, наконец,
-            запускает exec_loop
-        """
-        QTableWidget.__init__(self, args)
-
-        product_table = QTableWidget()
-        product_table.setRowCount(10)
-        product_table.setColumnCount(5)
-        product_table.setHorizontalHeaderLabels(['Наименование', 'Калорийность', 'Белки', 'Жиры', 'Углеводы'])
-
-
-if __name__ == "__main__":
-    import sys
-
-    app = QtWidgets.QApplication(sys.argv)
-    mw = MainWindow()
-    sys.exit(app.exec())
-
+window.show()
+sys.exit(app.exec_())
---
+++ mainWindow.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'G:/other/ambrosia/gui/mainWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.10
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(670, 435)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName("tabWidget")
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab")
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QtWidgets.QWidget()
+        self.tab_2.setObjectName("tab_2")
+        self.tabWidget.addTab(self.tab_2, "")
+        self.tab_3 = QtWidgets.QWidget()
+        self.tab_3.setObjectName("tab_3")
+        self.tabWidget.addTab(self.tab_3, "")
+        self.tab_4 = QtWidgets.QWidget()
+        self.tab_4.setObjectName("tab_4")
+        self.tabWidget.addTab(self.tab_4, "")
+        self.verticalLayout.addWidget(self.tabWidget)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 670, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Продукты"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Блюда"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("MainWindow", "Меню"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), _translate("MainWindow", "Закупка"))
+
